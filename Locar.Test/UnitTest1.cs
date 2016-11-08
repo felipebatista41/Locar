7518d0206f4a7473a1fd05e05c027006252c6233
SQZ36 <SQZ36@SQZ36>
2016-11-08 14:55:21 -0200

    Add project files.

---
+++ Locar.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Locar.Test
+{
+    [TestClass]
+    public class UnitTestLocar
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Locar.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Locar.Web.MvcApplication" Language="C#" %>
